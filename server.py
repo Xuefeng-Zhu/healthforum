157e44e89f887e1ad0e2c26506658005959569a4
Henry Lin <hlin117@mobile-172-16-104-148.near.illinois.edu>
2014-03-15 23:35:23 -0500

    heroku

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 
 from flask import Flask
+
+
 app = Flask(__name__)
 
+	
 @app.route("/")
 def index_page():
 	return "HELLO WORLD"
 
+"""
 
 if __name__ == "__main__":
 	app.run("0.0.0.0",8080,debug=True)
+
+"""
