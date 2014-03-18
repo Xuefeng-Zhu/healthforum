3675a4f6ea607d2dd5c74dd9867dfdc94a85a058
朱 雪峰 <Frank@wirelessprvnat-172-16-237-133.near.illinois.edu>
2014-03-18 12:16:58 -0500

    list

--- client/healtheffect/app/js/app.js
+++ client/healtheffect/app/js/app.js
@@ -14,7 +14,21 @@ angular.module('mgcrea.ngStrapDocs')
   $scope.icons = [
     {value: 'Relevance', label: '<i class="fa fa-plus"></i> Relevance'},
     {value: 'Letter', label: '<i class="fa fa-sort-alpha-asc"></i> Letter'},
-    {value: 'Most Review', label: '<i class="fa fa-comment"></i> Most Review'},
+    {value: 'Most Review', label: '<i class="fa fa-comment"></i> Most Review'}
+  ];
+
+  $scope.drugs = [
+  {name: 'A', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'B', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'C', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'D', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'E', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'F', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'G', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'H', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'I', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'J', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
+  {name: 'K', concise:'Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo Demo, Demo, Demo, Demo'},
   ];
 
 
