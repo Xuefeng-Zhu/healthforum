774a61221d941299fc37603d8a59dae7f6c63f82
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-03-18 13:20:45 -0500

    fix

--- client/healtheffect/app/js/app.js
+++ client/healtheffect/app/js/app.js
@@ -10,7 +10,6 @@ angular.module('mgcrea.ngStrapDocs')
 .controller('SelectDemoCtrl', function($scope, $http) {
 
   $scope.selectedIcon = '';
-  $scope.selectedIcons = ['Globe', 'Heart'];
   $scope.icons = [
     {value: 'Relevance', label: '<i class="fa fa-plus"></i> Relevance'},
     {value: 'Letter', label: '<i class="fa fa-sort-alpha-asc"></i> Letter'},
