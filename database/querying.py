871fbb4f56766fd56fe57e01609a69989043025d
shantanudev <shantanudev1@gmail.com>
2014-03-19 01:53:35 -0500

    json script

--- database/querying.py
+++ database/querying.py
@@ -14,15 +14,16 @@ name = db.cursor()
 name.execute("SELECT * FROM drugs where name=\"%s\""%drugName)
 for row in name.fetchall():
     drugid=row[0]
-    print "%s" % (row[1])
-print drugid
+    #print "%s" % (row[1])
+#print drugid
 side_effect=[]
 effect=db.cursor()
 effect.execute("SELECT * FROM side_effects where id=\"%d\""%drugid)
 for row in effect.fetchall():
 	side_effect.append(row[1])
 
-drugDictionary={}
-drugDictionary[drugName]=side_effect
+drugDictionary={"Drug": drugName , "SideEffects": side_effect}
+
+#print drugDictionary
 
 print json.dumps(drugDictionary)
