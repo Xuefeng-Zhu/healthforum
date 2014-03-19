0b6947d3fcc34078a5eccb433b7ea859c727ec31
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-03-19 00:05:56 -0500

    Comments

--- client/healtheffect/app/js/app.js
+++ client/healtheffect/app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('mgcrea.ngStrapDocs', ['ngRoute','ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
+var app = angular.module('myapp', ['ngRoute','ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
 
 app.config(['$routeProvider',
 	function($routeProvider) {
@@ -62,4 +62,24 @@ app.controller('SideEffectCtrl', function($scope, $http){
 	{name: 'A', links:[{name: 'Cnbeta', url:'http://www.Cnbeta.com'},{name: 'Google', url:'www.google.com'}]},
 	{name: 'B', links:[{name: 'Cnbeta', url:'http://www.Cnbeta.com'},{name: 'Google', url:'www.google.com'}]}
 	]
+});
+
+app.controller('CommentsCtrl', function($scope, $http){
+	$(document).ready(function() {
+		$('.summernote').summernote({
+			toolbar: [
+            ['style', ['style']], // no style button
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+            ['insert', ['picture', 'link']], // no insert buttons
+            ['table', ['table']], // no table button
+            ['help', ['help']] //no help button
+            ],
+            height: 150
+
+});	});
+
 })
