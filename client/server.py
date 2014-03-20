e12c03487c2fc770de55a0c8802516ca6cb11636
hlin117 <hlin117@gmail.com>
2014-03-19 22:08:26 -0500

    going to add client files

--- client/server.py
+++ client/server.py
@@ -5,17 +5,15 @@ from flask import render_template
 import os
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '')
-app = Flask(__name__, template_folder=tmpl_dir)
+app = Flask(__name__, template_folder = tmpl_dir)
 
-
-# Note to Self: Serving static content through Flask is not a good idea. host on actual webserver instead of app server
 @app.route("/")
 def search():
-	return render_template('search.html')
+	return render_template('/search.html')
 
 @app.route("/submit.js")
 def submitJS():
-	return render_template("submit.js")
+	return render_template("/submit.js")
 
 @app.route("/test",methods=['GET', 'POST'])
 def index_page():
