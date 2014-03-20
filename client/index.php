99de8884372bd5357b63a38b28b00039a2365863
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-03-20 02:00:22 -0500

    delete unnecessary files

--- client/index.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<head>
-<title>Drug Side Effect</title>
-</head>
-<body>
-	<h1 style="text-align:center; font-family:cursive">The drug side effect search.</h1>
-<p>Hello world. I am a paragraph</p>
-</body>
