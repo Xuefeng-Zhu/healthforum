bb5d266e64504617705a06de46180c47d1d320eb
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-03-20 01:54:35 -0500

    Merge branch 'master' of github.com:hlin117/healthforum
    Conflicts:
    	client/.DS_Store
    	client/index.php

---
+++ server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from flask import Flask
+from flask import request
+from flask import render_template
+import os
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+
+# Note to Self: Serving static content through Flask is not a good idea. host on actual webserver instead of app server
+@app.route("/")
+
+
+
+
+
+def search():
+	return render_template('client/search.html')
+
+@app.route("/client/submit.js")
+def submitJS():
+	return render_template("/client/submit.js")
+
+@app.route("/client/test",methods=['GET', 'POST'])
+def index_page():
+	if request.method=='POST':
+		return "hi"
+	else:
+		return "hiya"
+
+if __name__ == "__main__":
+	app.run("0.0.0.0", 80, debug=True)
