ac89e328f0cc1cee0ec4989dfb54e3e79f15cab9
hlin117 <hlin117@gmail.com>
2014-03-24 00:38:42 -0500

    server can run now

--- client/server.py
+++ client/server.py
@@ -9,19 +9,14 @@ app = Flask(__name__, template_folder = tmpl_dir)
 
 
 # Note to Self: Serving static content through Flask is not a good idea. host on actual webserver instead of app server
-
 @app.route("/")
 def index():
-    # template engine, hard.
-    return render_template('client/index.html',  )
+	return render_template('index.html')
 
 
-@app.route("/client/test", methods=['GET', 'POST'])
-def index_page():
-	if request.method=='POST':
-		return "hi"
-	else:
-		return "hiya"
+@app.route("/test",methods=['GET', 'POST'])
+def index_page(path):
+	return request.get(path)
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", 80, debug=True)
