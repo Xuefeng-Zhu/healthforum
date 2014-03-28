91d24dfe11a0dbe4e83894e54137ad7f9e7c0289
hlin117 <hlin117@gmail.com>
2014-03-28 17:24:28 -0500

    fixed, and now allows CORS. execessive commenting

--- runserver.py
+++ runserver.py
@@ -11,63 +11,86 @@ http://flask-restful.readthedocs.org/en/latest/installation.html
 
 More documentation on cors (cross origin resource sharing):
 https://github.com/twilio/flask-restful/pull/131
+
+More information on argument passing:
+http://flask-restful.readthedocs.org/en/latest/reqparse.html
 """
 
 app = Flask(__name__)
 api = restful.Api(app)
 api.decorators=[cors.crossdomain(origin='*')]
 
-DRUGS = [
-	{ 'name': 'drug a', 'drug a side effects': {'side effect': 'side effect a1', 'side effect': 'side effect a2' }},
-	{ 'name': 'drug b', 'drug b side effects': {'side effect': 'side effect b1', 'side effect': 'side effect b2' }},
-	{ 'name': 'drug c', 'drug c side effects': {'side effect': 'side effect c1', 'side effect': 'side effect c2' }}
+# Note this is a list of dictionaries.
+# Each dictionary has
+# 1. A string called "name"
+# 2. A list of side effects (strings) called "side effects"
+drugsTable = [
+	{ 'name': 'oxygen', 'side effects': ['side effect a1', 'side effect a2']},
+	{ 'name': 'humira', 'side effects': ['nausea', 'death']},
+	{ 'name': 'mcdonalds', 'side effects': ['irregular sleep habits', 'hysteria', 'uncontrollable shivering', 'vomiting', 'gaining weight']}
 ]
 
-#only output the .task. field
-fields = {
-	'drug': fields.String
-}
+# Generic function that aborts the call if the
+# queried index is not in the input list
+def assertInList(inputList, index):
+	if not(len(inputList) > index >= 0) or inputList[index] is None:
+		abort(404, message = "ERROR! Item doesn't exist in table.")
 
 # Drug
-#   show a single drug item and lets you delete them
+# show a single drug item and lets you delete them
 class Drug(restful.Resource):
 
-	# Note: There seems to be a way to you implement decorators
-	# on all of the classes, but I'm not sure how to do this. More information
-	# here: http://flask-restful.readthedocs.org/en/latest/extending.html#resource-method-decorators	
-	@marshal_with(fields)
-	def get(self, drug_id):
-		if not(len(DRUGS) > drug_id >= 0) or DRUGS[drug_id] is None:
-			abort(404, message="Drug {} doesn't exist".format(drug_id))
-		return DRUGS[drug_id]
+	# $ curl localhost:5000/drugs/2
+	def get(self, drugNum):
+		assertInList(drugsTable, drugNum)	
+		return drugsTable[drugNum]
 
-	def delete(self, drug_id):
-		if not(len(DRUGS) > drug_id >= 0):
-			abort(404, message="Drug {} doesn't exist".format(drug_id))
-		DRUGS[drug_id] = None
-		return "", 204
+	# $ curl localhost:5000/drugs/1 -X DELETE
+	def delete(self, drugNum):
+		assertInList(drugsTable, drugNum)	
+		drugsTable[drugNum] = None
+		return "Successfully killed.", 204
+
+api.add_resource(Drug, '/drugs/<int:drugNum>')
+
+
+# Allows the input URI to be parsed
+# We can theoretically use this for the above class, but we didn't
+parser = reqparse.RequestParser()
+parser.add_argument('name', type = str)
+parser.add_argument('side_effects', type = str, action = "append")	# Creates array
 
 # DrugList
-#   shows a list of all drugs, and lets you POST to add new drugs
-parser = reqparse.RequestParser()
-parser.add_argument('drug', type=str)
-
+# shows a list of all drugs, and lets you POST to add new drugs
 class DrugList(restful.Resource):
 
-	@marshal_with(fields)
+	# Gets the current list
+	# $ curl localhost:5000/drugs
 	def get(self):
-		return DRUGS
+		return drugsTable
 
+	# Adding a new drug and side effects list
+	# $ curl http://localhost:5000/drugs -d "name=advil" -d "side_effect=nausea" -d "side_effect=dying" -X POST -v
+	# Interesting note: When formulating your post request, don't have a space before
 	def post(self):
-		args = parser.parse_args()
-		task = {'drug': args['drug']}
-		DRUGS.append(task)
-		return marshal(task, fields), 201
 
+		# Parse the arguments from the post request
+		args = parser.parse_args()
+		name = args["name"]				# name = "advil"
+		effects = args["side_effect"] 	# effects = ["nausea", "dying"] 
+	
+		# Create the dictionary that we're going to put into the table
+		drugDict = dict()
+		drugDict["name"] = name
+		drugDict["side effects"] = effects
+
+		# Add this to the table and send its index to the client
+		drugsTable.append(drugDict)
+		drugNum = len(drugsTable) - 1
+		return drugNum, 201
 
 ## Actually setup the Api resource routing here
-api.add_resource(DrugList, '/drug')
-api.add_resource(Drug, '/drug/<int:drug_id>')
+api.add_resource(DrugList, '/drugs')
 
 if __name__ == '__main__':
 	app.run(debug=True)
