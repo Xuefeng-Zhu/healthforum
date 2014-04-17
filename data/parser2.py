3854f2dc0314d720383ddb083e59242ce002b4c2
hlin117 <hlin117@gmail.com>
2014-04-16 21:06:52 -0500

    added control statement

--- data/parser2.py
+++ data/parser2.py
@@ -22,19 +22,20 @@ with open('drugs2.txt','r') as f:
 			drugs_pat[drug_name] = shlex.split(line)
 
 
-## When testing
-#if True:
-#	db = MySQLdb.connect(host="engr-cpanel-mysql.engr.illinois.edu",
-#						 user="halin2_guest",
-#						 passwd="helloworld",
-#						 db="halin2_sample")
-#
-# For the actual server
-#else:
-db = MySQLdb.connect(host = "us-cdbr-east-05.cleardb.net",
-					user = "bbe6abd0b555dc",
-					passwd = "488c7e4d",
-					db = "heroku_5f9923672d3888a")
+# When testing
+if False:
+	db = MySQLdb.connect(host="engr-cpanel-mysql.engr.illinois.edu",
+						 user="halin2_guest",
+						 passwd="helloworld",
+						 db="halin2_sample")
+
+ For the actual server
+else:
+	db = MySQLdb.connect(host = "us-cdbr-east-05.cleardb.net",
+						user = "bbe6abd0b555dc",
+						passwd = "488c7e4d",
+						db = "heroku_5f9923672d3888a")
+
 # Side effects are now arranged in two dictionaries:
 # drugs_doct and drugs_pat. All we need to do now is
 # iterate through the drugs, and add the respective
