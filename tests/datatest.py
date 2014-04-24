5629c486ce085b19e41386bd20659ec96dd882a4
hlin117 <hlin117@gmail.com>
2014-04-24 00:28:55 -0500

    some comments on the tests

--- tests/datatest.py
+++ tests/datatest.py
@@ -39,38 +39,44 @@ def allDrugsTest():
 # actually, i'm not sure how to fail this (return 1), since nothing sets passed = 0, as long as one letter exists
 # might have to do something other than length of the list? it'd be stupid to fail if a beginsWith letter is false
 # HALP!
+
+# Henry: Hmm.... Maybe in this case, we'd just want to check "specific" consistency in the data.
+# For example, "clo" currently brings back 3 results. If this number changes, then
+# raise a warning. Otherwise we're good to go.
+# (You'd make siilar tests for different substrings)
 def alphaStartTest():
 	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-	passed = 0
 	try:
 		for i in alphabet:
 			drugs = curl(heroku + "drugs/result/" + i )
-			if len(drugs) >= 0:
-				passed = 1
-
-		if passed == 1:
-			return 0
-		return 1
+			if not len(drugs) >= 0:
+				return 1
+		return 0
 	except:
 		return 2
 
 # locate dummy data
 # alternatively, use this to locate other key words or phrases
+#
+# Henry: So actually, this function is going to return true everytime, because a lot of drugs have the following concise field:
+# "Hello world. This is somee information."
 def dummyData():
 	dummyText = ['hello world', 'add more dummy data from concise as element', 'avoid single words that could be in real entries']
 	drugs = curl(heroku + "drugs/all")
 	dummyTextExists = 0
 
+	# Henry: You might want to practice iterating upon the items in the list, not the
+	# indices of the list
+	# Python's special in that sense. Aim to write pythonic code!
+	# I'll let you try writing code that doesn't use indices. Have a go at it.
 	try:
 		for i in range (0, len(drugs)):
 			for j in range (0, len(dummyText)):
 				if dummyText[j].lower() in drugs[i]["concise"].lower():
-					dummyTextExists = 1
+					return 1
 					# we could return 99 here if we're just looking for the existence of dummy text instead of testing all data
 					# but we may want to log which arrays/data have dummy text so we can fix it
 
-		if dummyTextExists == 1:
-			return 1
 		return 0
 	except:
 		return 2
@@ -86,6 +92,7 @@ def patientData():
 			# get a list of all drug names in the db
 			drugNames.append (drugs[i]["name"])
 		
+		# Again, you don't have to iterate through the indices of the list, just the elements themselves.
 		for j in range (0, len(drugNames)):
 			# list of patient data per drug
 			pData =  curl(heroku + "drugs/" + drugNames[j] + "/patient")
@@ -103,13 +110,16 @@ def patientData():
 # is doctor side effect per drug empty
 def doctorData():
 	drugs = curl(heroku + "drugs/all")
-	drugNames = []
+#	drugNames = []
 	passed = 1
 
 	try:
-		for i in range (0, len(drugs)):
-			# get a list of all drug names in the db
-			drugNames.append (drugs[i]["name"])
+#	for i in range (0, len(drugs)):
+#			# get a list of all drug names in the db
+#			drugNames.append (drugs[i]["name"])
+
+		# More pythonic code - this time throwing something functional in there
+		drugNames = [drug["name"] for drug in drugs]
 		
 		for j in range (0, len(drugNames)):
 			# list of doctor data per drug
