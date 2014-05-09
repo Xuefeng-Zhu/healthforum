ab7d4459ddf6571a6cee82691915db87297830c4
hlin117 <hlin117@gmail.com>
2014-05-08 21:56:50 -0500

    updating forum link insertion script

--- data/insert_side_effects_details.py
+++ data/insert_side_effects_details.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from colorama import *
 import sys
 import json
 import MySQLdb
@@ -32,7 +33,6 @@ def getDrugId(drugName):
 	queryString = "select id from drugs where name='" + drugName + "'"
 	cursor.execute(queryString)
 	drug_id = cursor.fetchone()[0]
-	print "Found drug {0} with drug id {1}".format(drugName, drug_id)
 	return drug_id
 
 #def getSideEffectId(sideEffect, drug_id):
@@ -42,26 +42,54 @@ def getDrugId(drugName):
 #	side_effect_id = cursor.fetchone()[0]
 #	return side_effect_id
 #
-def insertPost(post, drug_id, effect):
-	queryString = "insert into side_effects_details(url, title, forum_id, content, drug_id, general_effect) values (\"{0}\", \"{1}\", {2}, \"{3}\", {4}, \"{5}\")" \
-					.format(post["url"].encode("utf-8"), post["title"].encode("utf-8"), post["forumId"], post["content"].encode("utf-8"), drug_id, effect.encode("utf-8"))
-	cursor.execute(queryString)
+def insertPost(post, drug_id, sideEffectId):
+	if post["forumId"] is None or post["forumId"] == "":
+		post["forumId"] = -1
+
+	queryString = "insert into side_effects_details(url, title, forum_id, content, side_effect_id) values (\"{0}\", \"{1}\", {2}, \"{3}\", {4})" \
+					.format(post["url"].encode("utf-8"), post["title"].encode("utf-8"), post["forumId"], post["content"].encode("utf-8"), sideEffectId)
+	try:
+		cursor.execute(queryString)
+	except Exception as e:
+		print "Error inserting post for the following query:"
+		print queryString
+		print "Error message: "+ str(e.message)
+		print ""
+
+def getSideEffectId(drug_id, sideEffectName, isDoctor):
+	effect = "doctor_effect" if isDoctor else "patient_effect"
+	queryString = "select id from side_effects where {0} = '{1}' and drug_id = {2}".format(effect, sideEffectName, drug_id)
+	try:
+		cursor.execute(queryString)
+		side_effect_id = cursor.fetchone()[0]
+		return side_effect_id
+	except Exception as e:
+		print "Error getting the side effect ID for the following query:"
+		print queryString
+		print "Error message: " + str(e.message) 
+		print ""
+		return None
+
 
 
 numInserted = 0
 
 for i, obj in enumerate(dataList):
-	try:
-		drugName = obj["drugName"]
-		drug_id = getDrugId(drugName);
-			
-		retrievedObjects = obj["retrievedObjects"]
-		for post in retrievedObjects:
-			insertPost(post, drug_id, obj["sideEffect"])
-			numInserted += 1
-		db.commit()
-		
-	except Exception as e:
-		print "Received an error with item number {0}, drug name {1} and side effect {2}".format(i, obj["drugName"], obj["sideEffect"])
+	drugName = obj["drugName"]
+	drug_id = getDrugId(drugName);
+
+	sideEffectName = obj["sideEffect"]
+	isDoctor = obj["isDrSERes"]
+	sideEffectId = getSideEffectId(drug_id, sideEffectName, isDoctor)
+	if sideEffectId is None:
 		continue
+		
+	retrievedObjects = obj["retrievedObjects"]
+	for post in retrievedObjects:
+		insertPost(post, drug_id, sideEffectId)
+		numInserted += 1
+	db.commit()
+		
+
+print str(numInserted) + " rows inserted."
 
